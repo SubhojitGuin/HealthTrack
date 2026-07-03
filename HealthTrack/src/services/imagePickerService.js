4dd3458e97e68221e69265c0f6b895b7fdaa6c33
Subhojit Guin <subhojitguin2004@gmail.com>
2026-07-03 16:35:17 +0530

    Update Profile

--- HealthTrack/src/services/imagePickerService.js
+++ HealthTrack/src/services/imagePickerService.js
@@ -1,6 +1,13 @@
 import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from "expo-file-system/legacy";
 
 export const pickImageFromGallery = async () => {
+  const permission = await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+  if (!permission.granted) {
+    throw new Error("Gallery permission denied");
+  }
+
   const result = await ImagePicker.launchImageLibraryAsync({
     mediaTypes: ImagePicker.MediaTypeOptions.Images,
     allowsEditing: true,
@@ -8,9 +15,24 @@ export const pickImageFromGallery = async () => {
   });
 
   if (!result.canceled) {
-    return result.assets[0].uri;
+    const image = result.assets[0];
+
+    const fileName = `gallery_${Date.now()}.jpg`;
+    const newUri = FileSystem.documentDirectory + fileName;
+
+    await FileSystem.copyAsync({
+      from: image.uri,
+      to: newUri,
+    });
+
+    const fileInfo = await FileSystem.getInfoAsync(newUri);
+
+    console.log("Original URI:", image.uri);
+    console.log("Saved URI:", newUri);
+    console.log("File Info:", fileInfo);
+
+    return newUri;
   }
 
   return null;
 };
-
